aaca8b600542d6aa9cff05e7bb525ecf3fb0c4a9
gerc <geisianeribeiro8@gmail.com>
2018-04-15 21:54:38 -0300

    Consertanto identação

--- ExercicioSimuladorPato/src/teste/SimuladorPato.java
+++ ExercicioSimuladorPato/src/teste/SimuladorPato.java
@@ -10,18 +10,18 @@ public class SimuladorPato {
 	public static void main(String[] args) {
 		
 		// Classe teste para simular patos voadores 
-				Pato pato1 = new PatoSelvagem();
-				pato1.exibir();
-				pato1.realizarVoo();
-				pato1.realizarGrasno();
+		Pato pato1 = new PatoSelvagem();
+		pato1.exibir();
+		pato1.realizarVoo();
+		pato1.realizarGrasno();
 				
-				Pato pato2 = new PatoMandarin();
-				pato2.exibir();
-				pato2.getVooComp();
-				
-				Pato patoModelo = new ModeloPato();
-				patoModelo.exibir();
-				patoModelo.ComFoguete();
+		Pato pato2 = new PatoMandarin();
+		pato2.exibir();
+		pato2.getVooComp();
+			
+		Pato patoModelo = new ModeloPato();
+		patoModelo.exibir();
+		patoModelo.ComFoguete();
 		
 	}
 
