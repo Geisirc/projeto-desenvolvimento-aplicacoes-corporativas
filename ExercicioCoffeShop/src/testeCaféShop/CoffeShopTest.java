a295d038e6b4d65730f1b030951ac1f18e3ef410
gerc <geisianeribeiro8@gmail.com>
2018-04-29 13:23:49 -0300

    Convertendo float com duas casas Decimais

--- ExercicioCoffeShop/src/testeCaféShop/CoffeShopTest.java
+++ ExercicioCoffeShop/src/testeCaféShop/CoffeShopTest.java
@@ -1,5 +1,9 @@
 package testeCaféShop;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+
 import servicos.Bebida;
 import servicos.Creme;
 import servicos.Expresso;
@@ -9,14 +13,16 @@ public class CoffeShopTest {
 	
 	public static void main(String args[]) {
 		
+		DecimalFormat decimal = new DecimalFormat("0.00");
+		
 		Bebida bebida = new Expresso();		
-		System.out.println(bebida.getDescription() + " R$ " + bebida.cost());
+		System.out.println(bebida.getDescription() + " R$ " + decimal.format(bebida.cost()));
 		
 		Bebida expresso = new Expresso();
 		expresso = new Leite(expresso);
 		expresso = new Leite(expresso);
 		expresso = new Creme(expresso);
-		System.out.println(expresso.getDescription() + " R$ " + expresso.cost());
+		System.out.println(expresso.getDescription() + " R$ " + decimal.format(expresso.cost()));
 		
 		
 	}
